5207b8c7b6a9f2b93055118ce4632752a6aeaddd
Gunjan-Yadav12 <157803078+Gunjan-Yadav12@users.noreply.github.com>
2026-06-10 01:12:23 +0530

    Solved Rotate_Array

---
+++ Arrays/Rotate_Array.cpp
@@ -0,0 +1,34 @@
+/*
+Problem: Rotate Array
+Platform: LeetCode
+
+Approach:
+1. Calculate k % n because rotating n times
+   brings the array back to its original position.
+2. Store the last k elements in a temporary array.
+3. Shift the remaining elements k positions to the right.
+4. Copy the stored elements to the first k positions.
+
+Key Idea:
+Break the array into two parts:
+*/
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k % n;
+
+        vector<int>temp(k);
+
+        for(int i=n-k; i<n; i++){
+            temp[i-(n-k)] = nums[i];
+        }
+        for(int i= n-k-1; i>=0; i--){
+            nums[i+k] = nums[i];
+        }
+        for(int i=0; i<k ; i++){
+            nums[i] = temp[i];
+        }
+    }
+};
