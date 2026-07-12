f51863bc4b1fc953890ecdc0b0a7d147718186e9
Gunjan-Yadav12 <157803078+Gunjan-Yadav12@users.noreply.github.com>
2026-07-12 19:18:26 +0530

    Floor and Ceil in sorted array problem

---
+++ Arrays/floor-ceil.cpp
@@ -0,0 +1,118 @@
+/*
+Pattern:
+Binary Search
+Lower Bound
+
+Initialize:
+
+low = 0
+high = n - 1
+ans = n
+
+While:
+
+low <= high
+
+Find middle:
+
+mid = low + (high - low) / 2
+
+If:
+
+nums[mid] >= target
+
+Store possible answer:
+
+ans = mid
+
+Search Left Half:
+
+high = mid - 1
+
+Else:
+
+Search Right Half:
+
+low = mid + 1
+
+Answer:
+
+ans
+
+Why does it work?
+
+We are looking for the
+first position where
+
+nums[index] >= target.
+
+If target exists,
+its index is returned.
+
+If target doesn't exist,
+the first element greater
+than target becomes the
+correct insertion position.
+
+If every element is
+smaller than target,
+
+ans remains n,
+
+meaning insert at the end.
+
+Time: O(log n)
+
+Space: O(1)
+*/
+
+
+class Solution {
+public:
+    int floor(vector<int>&nums, int x){
+        int n = nums.size();
+        int low = 0;
+        int high = n-1;
+        int ans = -1;
+
+        while(low <= high){
+            int mid = (low + high)/2;
+
+            if(nums[mid] <= x){
+                ans = nums[mid];
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+    int ceil(vector<int>& nums, int x){
+        int n = nums.size();
+        int low = 0;
+        int high = n-1;
+        int ans = -1;
+        
+        while(low <= high){
+            int mid = (low + high)/2;
+
+            if(nums[mid] >= x){
+                ans = nums[mid];
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+    vector<int> getFloorAndCeil(vector<int> nums, int x) {
+        
+        int Floor = floor(nums , x);
+        int Ceil = ceil(nums , x);
+
+        return {Floor , Ceil};
+
+    }
+};
