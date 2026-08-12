e3a61982f07508c8bb98b02ca97b849e2ba3ef14
Gunjan-Yadav12 <157803078+Gunjan-Yadav12@users.noreply.github.com>
2026-08-13 01:18:59 +0530

    Solved  Kth Missing Positive Number problem

---
+++ Arrays/cap_ship_package_days.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int Days(vector<int>& weights ,  int capacity){
+
+        int days = 1;
+        int load = 0;
+
+        for(int i=0; i<weights.size(); i++){
+
+            if(load + weights[i] > capacity){
+
+                days = days + 1;
+                load = weights[i];
+            }
+            else{
+                load += weights[i];
+            }
+        }
+        return days;
+    }
+
+    int shipWithinDays(vector<int>& weights, int days) {
+        int low = *max_element(weights.begin() , weights.end());
+        int sum = 0;
+
+        for(int i = 0; i<weights.size(); i++){
+
+            sum += weights[i];
+        }
+        int high = sum;
+
+        while(low <= high){
+            int mid = (low + high)/2;
+
+            int numberOfDays = Days(weights , mid);
+
+            if(numberOfDays <= days){
+                 high = mid - 1;
+            }
+            else{
+                 low = mid + 1;
+            }
+        }
+        return low;
+        
+    }
+};
